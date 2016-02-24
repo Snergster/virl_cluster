5f8c68dbaacbf9189e4dd8040903a397c75b4205
Ed Kern <ejkern@mac.com>
2016-02-24 10:05:08 -0700

    numbered output for ordering

--- output.tf
+++ output.tf
@@ -1,18 +1,18 @@
-    output "ip" {
+    output "4. VIRL controller ip" {
       value = "${packet_device.virl.network.0.address}"
     }
-    output "uwm" {
+    output "5. uwm without OpenVPN" {
       value = "http://${packet_device.virl.network.0.address}:19400"
     }
-    output "uwm login" {
+    output "6. uwmadmin login" {
       value = "login uwmadmin password ${var.uwmadmin_password}"
     }
-    output "guest login" {
+    output "3. guest login" {
       value = "login guest password ${var.guest_password}"
     }
-    output "OpenVPN" {
-        value = "Your OpenVPN client connection file is now available at ./client.ovpn"
+    output "1. OpenVPN client file" {
+        value = "Your OpenVPN client connection file is now available at ./cluster.ovpn"
     }
-    output "uwm over OpenVPN" {
+    output "2. uwm with OpenVPN" {
       value = "http://172.16.11.254:19400"    
     }
